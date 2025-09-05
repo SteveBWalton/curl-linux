ad232e8f4553f864e2585125a40256d93ef3a82d
Steve Walton <37951256+SteveBWalton@users.noreply.github.com>
2025-09-05 08:18:07 +0100

    Minor changes.

--- curl-test.cpp
+++ curl-test.cpp
@@ -26,7 +26,7 @@ int main
     std::cout << "Hello from curl-test." << std::endl;
 
     // Create a HttpSimple object.
-    std::unique_ptr<HttpSimple> httpToken (new HttpSimple("http://192.168.2.1:8080/CIA/General/BearerToken", NULL, "{\"param1\":\"value1\",\"param2\":\"value2\"}\n", false));
+    std::unique_ptr<HttpSimple> httpToken (new HttpSimple("http://192.168.2.1:/GetToken", NULL, "{\"param1\":\"value1\",\"param2\":\"value2\"}\n", false));
 
     if (httpToken->getResponseCode() == 200)
     {
@@ -39,7 +39,7 @@ int main
     }
 
     // Create a http simple object for PartInfo.
-    std::unique_ptr<HttpSimple> httpPartInfo (new HttpSimple("http://192.168.2.1:8080/CIA/Blades/PartInfo", "Authorization: Bearer code-here", "{\"param1\":\"value1\",\"param2\":\"value2\"}\n", true));
+    std::unique_ptr<HttpSimple> httpPartInfo (new HttpSimple("http://192.168.2.1:/GetInfo", "Authorization: Bearer code-here", "{\"param1\":\"value1\",\"param2\":\"value2\"}\n", true));
 
     if (httpPartInfo->getResponseCode() == 200)
     {
